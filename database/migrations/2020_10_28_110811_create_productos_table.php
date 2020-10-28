7bcc93d8c38ffc9757e63bf7efb02ef351ec8185
Sara Álvarez Turrión <sara_sat93@hotmail.com>
2020-10-28 12:45:53 +0100

    api_rest nerin_soap creado produtos

---
+++ database/migrations/2020_10_28_110811_create_productos_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('productos', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('img');
+            $table->string('description');
+            $table->double('price');
+            $table->string('category');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('productos');
+    }
+}
